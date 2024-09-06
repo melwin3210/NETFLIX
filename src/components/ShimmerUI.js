a81007de73db2ccbbd9946243184f135ab8f6659
melwin <melwin3210@gmail.com>
2024-09-06 17:38:43 +0530

    shimmer UI

--- src/components/ShimmerUI.js
+++ src/components/ShimmerUI.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
 
 const ShimmerUI = () => {
   return (
-    <div>
-        <Header/>
-        <h1 className='font-black'>shimmerr............</h1>
-      
+    <div className=" bg-gradient-to-b from-black">
+        <Header />
+      <div class="flex space-x-2 justify-center items-center  h-screen dark:invert">
+        <span class="sr-only">Loading...</span>
+        <div class="h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+        <div class="h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+        <div class="h-8 w-8 bg-white rounded-full animate-bounce"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShimmerUI
+export default ShimmerUI;
