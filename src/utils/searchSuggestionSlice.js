8ccb7bca6c62c5b39970841813e93c4afc7c0e49
melwin <melwin3210@gmail.com>
2024-09-10 18:49:33 +0530

    duplicate keys in object removed in suggestion cache

--- src/utils/searchSuggestionSlice.js
+++ src/utils/searchSuggestionSlice.js
@@ -3,35 +3,39 @@ import { createSlice } from "@reduxjs/toolkit";
 const searchSuggestionSlice = createSlice({
   name: "searchSuggestion",
   initialState: {
-
-    suggestionsCache:{}
+    suggestionsCache: {},
   },
   reducers: {
-
     addSearchSuggestionsCache: (state, action) => {
-      state.suggestionsCache = cacheStorageCheck(state.suggestionsCache,action.payload) 
+      state.suggestionsCache = cacheStorageCheck(
+        state.suggestionsCache,
+        action.payload
+      );
     },
   },
 });
 
 const cacheStorageCheck = (cacheList, latestUpdate) => {
-  let copyList = {...cacheList}
-  let list = {}
-  
-  if(Object.keys(copyList).length>100){
+  const duplicateKeys = Object.keys(latestUpdate).filter(
+    (key) => key in cacheList
+  );
+
+  if (duplicateKeys.length > 0) {
+    return cacheList;
+  }
+  let copyList = { ...cacheList };
+  let list = {};
+
+  if (Object.keys(copyList).length > 100) {
     let firstKey = Object.keys(copyList)[0];
-    
+
     // Delete the first key-value pair
     delete copyList[firstKey];
-
   }
- return list = {...copyList, ...latestUpdate}
-  
-}
 
+  return (list = { ...copyList, ...latestUpdate });
+};
 
-
-
-export const {   addSearchSuggestionsCache} = searchSuggestionSlice.actions;
+export const { addSearchSuggestionsCache } = searchSuggestionSlice.actions;
 
 export default searchSuggestionSlice.reducer;
